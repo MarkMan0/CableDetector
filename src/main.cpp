bc251944aad034ba791a627f494b543a21520ce5
pal.mark28 <pal.mark28@gmail.com>
2019-04-18 17:20:20 +0200

    volt reading on the coil

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include "ADC_ADS1015.h"
+
+#define DEBUG 1
+
+//variable to be used in the ISR
+float reading; 
+
+
+ADC_ADS1015 adc;
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  #ifdef DEBUG
+    Serial.begin(115200);
+  #endif
+
+
+  adc.startContinuous(MUXA0A1, PGA02, DR3300);
+  reading = 0.0;
+  //TODO: set up timerX for discrete sampling
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  //a reading is taken every loop, can't be done in an ISR, because it freezes the code
+  //TODO: solve it
+  reading = adc.readVolts();    
+  
+  #ifdef DEBUG
+    Serial.print("reading: ");
+    Serial.println(reading);
+  #endif
+
 }
