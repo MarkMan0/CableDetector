072706f24e69b92b70b59f464013f339dd3b3652
pal.mark28 <pal.mark28@gmail.com>
2019-04-21 22:18:41 +0200

    tested OLED

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ float reading;
 
 ADC_ADS1015 adc;
 
+Adafruit_SSD1306 disp(128, 64);
 
 
 void setup() {
@@ -26,6 +27,10 @@ void setup() {
   #endif
 
 
+  disp.begin(SSD1306_SWITCHCAPVCC, DISP_ADDR);
+  disp.clearDisplay();
+  disp.setTextColor(WHITE);
+  disp.display();
   adc.startContinuous(MUXA0A1, PGA02, DR3300);
   reading = 0.0;
   //TODO: set up timerX for discrete sampling
@@ -37,7 +42,13 @@ void loop() {
   //a reading is taken every loop, can't be done in an ISR, because it freezes the code
   //TODO: solve it
   reading = adc.readVolts();    
+
+  disp.setCursor(0,1);
+  disp.setTextSize(2);
   
+  disp.println("TEST");
+  disp.display();
+
   #ifdef DEBUG
     Serial.print("reading: ");
     Serial.println(reading);
