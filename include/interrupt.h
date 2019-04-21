42f69ff37f1b9fbd5a992ca57d24413e768fbb71
pal.mark28 <pal.mark28@gmail.com>
2019-04-22 00:05:14 +0200

    fourier transform implemented

--- include/interrupt.h
+++ include/interrupt.h
@@ -3,7 +3,11 @@
 extern double freq;
 extern unsigned long last;
 
-ISR(TIMER2_COMPA_vect){
-    freq = 1000000.0/(micros() - last);
-    last = micros();
+extern Fourier fourier;
+extern float reading;
+
+ISR(TIMER1_COMPA_vect){
+    fourier.c += reading*cos(2.0*PI*50.0*fourier.count*TS) * TS;
+    fourier.s += reading*sin(2.0*PI*50.0*fourier.count*TS) * TS;
+    ++fourier.count;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 
 #define DISP_ADDR   (0x3C)
 
+#define TS (1.0/5000.0)
+
+#define READING_CNT   (5000)
+
+struct Fourier {
+  double s, c;
+  unsigned long count = 0;
+};
+
 #include "interrupt.h"
 double freq = 0.0;
 unsigned long last = 0;
@@ -23,9 +32,7 @@ ADC_ADS1015 adc;
 
 Adafruit_SSD1306 disp(128, 64);
 
-struct Fourier {
-  double s, c;
-};
+
 
 Fourier fourier;
 
@@ -45,17 +52,18 @@ void setup() {
   
   //setup interrupt for timer2
   cli(); // stop interrupts
-  TCCR2A = 0; // set entire TCCR2A register to 0
-  TCCR2B = 0; // same for TCCR2B
-  TCNT2  = 0; // initialize counter value to 0
+  TCCR1A = 0; // set entire TCCR2A register to 0
+  TCCR1B = 0; // same for TCCR2B
+  TCNT1  = 0; // initialize counter value to 0
   // set compare match register for 1000 Hz increments
-  OCR2A = 249; // = 16000000 / (64 * 1000) - 1
+  OCR1A = 999; // = 16000000 / (8 * 5000) - 1
+  
   // turn on CTC mode
-  TCCR2B |= (1 << WGM22);
-  // Set CS22, CS21 and CS20 bits for 64 prescaler
-  TCCR2B |= (1 << CS22) | (0 << CS21) | (0 << CS20);
+  TCCR1B |= (1 << WGM12);
+  // Set CS22, CS21 and CS20 bits for 32 prescaler
+  TCCR1B |= (1 << CS11) | (0 << CS10);
   // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
+  TIMSK1 |= (1 << OCIE1A);
   sei(); // allow interrupts
 }
 
@@ -63,21 +71,28 @@ void loop() {
   
   //a reading is taken every loop, can't be done in an ISR, because it freezes the code
   //TODO: solve it
-  Serial.println(freq);
-  return;
+  
+  if(fourier.count > READING_CNT) {
+    //cli();  //stop interrupt
+    TCNT1  = 0; //reset counter
 
+    float magn = 1000000*sqrt(fourier.c*fourier.c + fourier.s*fourier.s);
+
+    disp.setCursor(0,1);
+    disp.setTextSize(2);
+  
+    disp.println("TEST");
+    disp.display();
+    Serial.println(magn);
+    fourier.c = 0;
+    fourier.s = 0;
+    fourier.count = 0;
+    //sei();
+  }
+  
   reading = adc.readVolts();    
 
-  disp.setCursor(0,1);
-  disp.setTextSize(2);
   
-  disp.println("TEST");
-  disp.display();
-
-  #ifdef DEBUG
-    Serial.print("reading: ");
-    Serial.println(reading);
-  #endif
 
 }
 
